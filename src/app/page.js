a28acbf6de8b45b46b3742148b32c3edb061a902
MdBobby <mndbobby61@gmail.com>
2026-06-25 02:45:59 +0600

    almost homepage done

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,16 @@ import Banner from "@/components/home/Banner";
 import FeaturedProperties from "@/components/home/FeaturedProperties";
 import Reviews from "@/components/home/Reviews";
 import TopLocations from "@/components/home/TopLocations";
-import WhyChooseUs from "@/components/home/Whychooseus";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
 import SearchBar from "@/components/home/SearchBar";
 import Image from "next/image";
+import RecentProperties from "@/components/home/RecentProperties";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      {/* <SearchBar /> */}
+      <SearchBar />
       <FeaturedProperties />
       <WhyChooseUs />
       <Reviews />
